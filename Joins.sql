0ec2451a998c53615fa5a7b0bc3ea94c14246e41
Konya <quintaachiengotieno@gmail.com>
2024-07-29 10:51:32 +0300

    uploaded SQL files

---
+++ Joins.sql
@@ -0,0 +1,5 @@
+create database CompanyDatabase;
+drop database CompanyDatabase;
+
+use companydb;
+sel
---
+++ Select.sql
@@ -0,0 +1,9 @@
+USE companydb;
+
+SELECT * FROM employees;
+
+SELECT FirstName,LastName FROM employees;
+
+SELECT * FROM departments;
+
+SELECT FirstName,LastName FROM employees WHERE DepartmentID = 102;
